746d5b98fb4bfb282c9bea7f331060bd1eca6a99
FlArEs <paul2314@naver.com>
2018-02-23 19:44:24 +0900

    부드러운 움직임 구현
    - Ease In & Ease Out & SmoothStep 함수 구현
    - Singleton 스크립트 Utility 폴더로 경로 변경

---
+++ Assets/Scripts/Utility/ExpMath.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+
+public class ExpMath {
+	/// <summary>
+	/// 1단계 Ease In
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float EaseIn1(float t) {
+		if (t >= 0f && t <= 1f)
+			return 1f - Mathf.Cos(t * Mathf.PI * 0.5f);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 2단계 Ease In
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float EaseIn2(float t) {
+		if (t >= 0f && t <= 1f)
+			return t * t;
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 1단계 Ease Out
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float EaseOut1(float t) {
+		if (t >= 0f && t <= 1f)
+			return Mathf.Sin(t * Mathf.PI * 0.5f);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 2단계 Ease Out
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float EaseOut2(float t) {
+		if (t >= 0f && t <= 1f)
+			return Mathf.Sqrt(t);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 1단계 SmoothStep
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float SmoothStep1(float t) {
+		if (t >= 0f && t <= 1f)
+			return t * t * (3 - 2 * t);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 2단계 SmoothStep
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float SmoothStep2(float t) {
+		if (t >= 0f && t <= 1f)
+			return t * t * t * (t * (6f * t - 15f) + 10f);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 3단계 SmoothStep
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float SmoothStep3(float t) {
+		if (t >= 0f && t <= 1f)
+			return t * t * t * t * (t * (t * (-20f * t + 70f) - 84f) + 35f);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 4단계 SmoothStep
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float SmoothStep4(float t) {
+		if (t >= 0f && t <= 1f)
+			return t * t * t * t * t * (t * (t * (t * (70f * t -315f) + 540f) - 420f) + 126f);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 5단계 SmoothStep
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float SmoothStep5(float t) {
+		if (t >= 0f && t <= 1f)
+			return t * t * t * t * t * t * (t * (t * (t * (t * (-252f * t + 1386f) - 3080f) + 3465f) - 1980f) + 462f);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+
+	/// <summary>
+	/// 6단계 SmoothStep
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public float SmoothStep6(float t) {
+		if (t >= 0f && t <= 1f)
+			return t * t * t * t * t * t * t * (t * (t * (t * (t * (t * (924f * t - 6006f) + 16380f) - 24024f) + 20020f) - 9009f) + 1716f);
+		else if (t < 0f)
+			return 0;
+		else
+			return 1;
+	}
+}
