1fa79b1fae557699f73bf7133204f170f5d6578f
FlArEs <paul2314@naver.com>
2018-03-10 09:22:33 +0900

    에디터 - 커맨드 오브젝트 생성 기능 구현
    에디터 콘텐트 오브젝트 위에서 마우스로 우클릭하면 해당 위치에 커맨드 오브젝트 생성

--- Assets/Scripts/Manage/EditManageScript.cs
+++ Assets/Scripts/Manage/EditManageScript.cs
@@ -2,17 +2,27 @@
 using UnityEngine.UI;
 
 public class EditManageScript : MonoBehaviour {
-	
+
+	private const string MouseScrollWheel = "Mouse ScrollWheel";
+
 	public RectTransform editorContent;
 	public Scrollbar scrollbar;
+	public GameObject commandPrefab;
 
 	private float sizeRate = 1.12f;
+	private float contentMoveSpeed = 0.04f;
 
 	private float size = 1f;
 
 	private void Update() {
+		MouseScroll();
+		LeftRightMove();
+		EditorContentMouseClick();
+	}
+
+	private void MouseScroll() {
 		if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))) {
-			if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+			if (Input.GetAxis(MouseScrollWheel) > 0) {
 				// 위로 스크롤
 				editorContent.localScale = new Vector3(sizeRate * editorContent.localScale.x, 1, 1);
 				RectTransform[] childContents = editorContent.GetComponentsInChildren<RectTransform>();
@@ -22,7 +32,7 @@ public class EditManageScript : MonoBehaviour {
 				}
 				size *= sizeRate;
 			}
-			else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+			else if (Input.GetAxis(MouseScrollWheel) < 0) {
 				// 아래로 스크롤
 				if (size >= sizeRate) {
 					editorContent.localScale = new Vector3((1 / sizeRate) * editorContent.localScale.x, 1, 1);
@@ -33,6 +43,24 @@ public class EditManageScript : MonoBehaviour {
 					}
 					size *= (1 / sizeRate);
 				}
+			}
+		}
+	}
+
+	private void LeftRightMove() {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			scrollbar.value += contentMoveSpeed / size;
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow)) {
+			scrollbar.value -= contentMoveSpeed / size;
+		}
+	}
+
+	private void EditorContentMouseClick() {
+		if (Input.GetMouseButtonDown(1)) {
+			if (Input.mousePosition.y >= 0 && Input.mousePosition.y <= 300) {
+				GameObject commandObj = Instantiate(commandPrefab, new Vector3(Input.mousePosition.x, 150, 0), Quaternion.identity, editorContent) as GameObject;
+				commandObj.transform.localScale = new Vector3(1 / size, 1, 1);
 			}
 		}
 	}
