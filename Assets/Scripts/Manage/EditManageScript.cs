148768dfd6ea99ddef9ce78009cc8085150c990c
FlArEs <paul2314@naver.com>
2018-03-10 10:33:33 +0900

    SizeRateText 추가
    하핫

--- Assets/Scripts/Manage/EditManageScript.cs
+++ Assets/Scripts/Manage/EditManageScript.cs
@@ -7,12 +7,15 @@ public class EditManageScript : MonoBehaviour {
 
 	public RectTransform editorContent;
 	public Scrollbar scrollbar;
+	public Text sizeRateText;
 	public GameObject commandPrefab;
 
 	private float sizeRate = 1.12f;
 	private float contentMoveSpeed = 0.04f;
 
 	private float size = 1f;
+
+	private Color textColor;
 
 	private void Update() {
 		MouseScroll();
@@ -23,25 +26,37 @@ public class EditManageScript : MonoBehaviour {
 	private void MouseScroll() {
 		if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))) {
 			if (Input.GetAxis(MouseScrollWheel) > 0) {
-				// 위로 스크롤
-				editorContent.localScale = new Vector3(sizeRate * editorContent.localScale.x, 1, 1);
-				RectTransform[] childContents = editorContent.GetComponentsInChildren<RectTransform>();
-				foreach (var content in childContents) {
-					if (content.CompareTag("Command"))
-						content.localScale = new Vector3((1 / sizeRate) * content.localScale.x, 1, 1);
-				}
-				size *= sizeRate;
-			}
-			else if (Input.GetAxis(MouseScrollWheel) < 0) {
-				// 아래로 스크롤
-				if (size >= sizeRate) {
-					editorContent.localScale = new Vector3((1 / sizeRate) * editorContent.localScale.x, 1, 1);
+				if (size * sizeRate <= 10000f) {
+					// 위로 스크롤
+					editorContent.localScale = new Vector3(sizeRate * editorContent.localScale.x, 1, 1);
 					RectTransform[] childContents = editorContent.GetComponentsInChildren<RectTransform>();
 					foreach (var content in childContents) {
 						if (content.CompareTag("Command"))
-							content.localScale = new Vector3(sizeRate * content.localScale.x, 1, 1);
+							content.localScale = new Vector3((1 / sizeRate) * content.localScale.x, 1, 1);
 					}
-					size *= (1 / sizeRate);
+					size *= sizeRate;
+
+					textColor = sizeRateText.color;
+					sizeRateText.color = new Color(textColor.r, textColor.g, textColor.b, 1);
+					sizeRateText.text = Mathf.Round((size * 100)).ToString() + "%";
+				}
+			}
+			else if (Input.GetAxis(MouseScrollWheel) < 0) {
+				if (size >= 1) {
+					// 아래로 스크롤
+					if (size >= sizeRate) {
+						editorContent.localScale = new Vector3((1 / sizeRate) * editorContent.localScale.x, 1, 1);
+						RectTransform[] childContents = editorContent.GetComponentsInChildren<RectTransform>();
+						foreach (var content in childContents) {
+							if (content.CompareTag("Command"))
+								content.localScale = new Vector3(sizeRate * content.localScale.x, 1, 1);
+						}
+						size *= (1 / sizeRate);
+
+						textColor = sizeRateText.color;
+						sizeRateText.color = new Color(textColor.r, textColor.g, textColor.b, 1);
+						sizeRateText.text = Mathf.Round((size * 100)).ToString() + "%";
+					}
 				}
 			}
 		}
---
+++ Assets/Scripts/TextCtrl/SizeRateTextCtrl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SizeRateTextCtrl : MonoBehaviour {
+
+	private Text textComp;
+	private Color textColor;
+
+	private float alphaInterval = 0.02f;
+
+	private void Awake() {
+		textComp = gameObject.GetComponent<Text>();
+	}
+
+	private void Update() {
+		textColor = textComp.color;
+		if (textColor.a > 0) {
+			textComp.color = new Color(textColor.r, textColor.g, textColor.b, textColor.a - alphaInterval);
+		}
+		else {
+			textComp.color = new Color(textColor.r, textColor.g, textColor.b, 0);
+		}
+	}
+}
